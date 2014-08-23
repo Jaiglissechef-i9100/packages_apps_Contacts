LineageOS/android_packages_apps_Contacts

contacts: Holo Light ringtone picker dialog
Depends on
http://review.cyanogenmod.org/#/c/61392
http://review.cyanogenmod.org/#/c/61393

Change-Id: I2619e88a2ab573c18acae3bc05887d5ec3038666
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

src/com/android/contacts/detail/ContactLoaderFragment.java
@@ -426,6 +426,8 @@ public class ContactLoaderFragment extends Fragment implements FragmentKeyListen
         intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_RINGTONE);
         // Allow the user to pick a silent ringtone
         intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, true);
+        // Set HoloLight theme dialog
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_DIALOG_THEME, R.style.Theme_Ringtone_Dialog);
 
         Uri ringtoneUri;
         if (mCustomRingtone != null) {

Jaiglissechef-i9100/packages_apps_Contacts

src/com/android/contacts/detail/ContactLoaderFragment.java
@@ -426,6 +426,8 @@ public class ContactLoaderFragment extends Fragment implements FragmentKeyListen
         intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_RINGTONE);
         // Allow the user to pick a silent ringtone
         intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, true);
+        // Set HoloLight theme dialog
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_DIALOG_THEME, R.style.Theme_RingtoneDialog);
 
         Uri ringtoneUri;
         if (mCustomRingtone != null) {